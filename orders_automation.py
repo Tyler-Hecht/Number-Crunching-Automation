7233c0272948ecd6d8fb75b3aa19eed4fd31f1b3
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-01-24 17:00:21 -0500

    Program can now recognize common errors

--- orders_automation.py
+++ orders_automation.py
@@ -6,12 +6,28 @@ import glob
 #open the xlsx file
 path = os.getcwd()
 file = glob.glob(os.path.join(path, "*.xlsx"))
+# handles errors with xlsx file
+if len(file) == 0:
+    print("ERROR: No .xlsx files found")
+    exit()
+if len(file) > 1:
+    # allows the .xlsx file to be open while running the program
+    if not (len(file) == 2 and (file[0].split("\\")[-1] == file[1].split("\\")[-1][2:])):
+        print("ERROR: Multiple .xlsx files in folder")
+        exit()
+# if no errors
 file_name = file[0].split("\\")[-1]
 xls = ExcelFile(file_name)
 
-#handle the trialtypes file
-with open("trialtypes.txt") as f:
-    txt = f.readlines()
+# finds trialtypes.txt
+try:
+    with open("trialtypes.txt") as f:
+        txt = f.readlines()
+# if there is no trialtypes.txt
+except:
+    print("ERROR: trialtypes.txt not found. Make sure the file is properly named.")
+    exit()
+# gets the important information from trialtypes.txt
 trial_types = {}
 for line in txt:
     # ignores the lines about frames
@@ -32,6 +48,10 @@ def mirror(str: str) -> str:
 
 # iterates over every sheet (order)
 for sheet in xls.sheet_names:
+    # makes sure the sheet is named correctly
+    if not "Order" in sheet:
+        print("ERROR: sheet in .xlsx file named incorrectly. Make sure all the sheets are named \"Order i\" such that i is a single digit.")
+        exit()
     # converts the sheet to a dictionary
     df = xls.parse(sheet)
     bad_dict = (df.to_dict())
@@ -39,17 +59,27 @@ for sheet in xls.sheet_names:
     # creates a list of the cells in the two relevant columns
     trial_type = []
     participant_looking_location = []
-    for key in bad_dict["Trial Type"]:
-        trial_type.append(bad_dict["Trial Type"][key])
-    for key in bad_dict["Participant Looking Location"]:
-        participant_looking_location.append(bad_dict["Participant Looking Location"][key])
+    try:
+        for key in bad_dict["Trial Type"]:
+            trial_type.append(bad_dict["Trial Type"][key])
+        for key in bad_dict["Participant Looking Location"]:
+            participant_looking_location.append(bad_dict["Participant Looking Location"][key])
+    except:
+        print("Columns in .xlsx file named incorrectly. Make sure to call the second and third columns \"Trial Type\" and \"Participant Looking Location\", respectively.")
+        exit()
 
     # builds the txt file
     text = ""
     i = 0
     for trial in trial_type:
-        text += mirror(participant_looking_location[i]) + " " + str(trial_types[trial]) + "\n"
+        text += mirror(participant_looking_location[i]) + " "
         i += 1
+        # finds errors in column names
+        try:
+            text += str(trial_types[trial]) + "\n"
+        except:
+            print("ERROR: Trial type found in .xlsx file which is not recognized by participants.txt")
+            exit()
     
     # creates a txt file
     name = "order" + sheet[-1] + ".txt"
