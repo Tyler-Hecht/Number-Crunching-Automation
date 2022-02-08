2f14f7c06aa24875bd6c842294ad8c13735ae03b
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-02-08 16:28:13 -0500

    Update run.sh

--- Number_Crunching_Automation/run.sh
+++ Number_Crunching_Automation/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+cd orders 
 python3 ./trialtypes_automation.py 
 python3 ./orders_automation.py 
+cd .. 
+cd input 
 python3 xlsx_to_txt.py 
 python3 participants_automation.py 
+cd .. 
 javac Cruncher.java 
 java Cruncher 
 echo NUMBER CRUNCHING COMPLETE!
