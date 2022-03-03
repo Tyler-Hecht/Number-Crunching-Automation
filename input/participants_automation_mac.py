8cb98c16c82c5e835ff302cb3ccd9f775de35286
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-03-03 14:58:08 -0500

    Update participants_automation_mac.py

--- input/participants_automation_mac.py
+++ input/participants_automation_mac.py
@@ -15,11 +15,11 @@ try:
 # if there aren't any .xlsx files
 except:
     print("ERROR: No .xlsx files found")
-    exit()
+    exit(1)
 # makes sure there's only one order sheet
 if len(order_sheets) == 0:
     print("ERROR: No order sheet found. Make sure it is an .xlsx containing the words \"order\" and \"sheet\".")
-    exit()
+    exit(1)
 if len(order_sheets) > 1:
     # allows the .xlsx file to be open while running the program
     if not (len(order_sheets) == 2 and (order_sheets[0].split("/")[-1] == order_sheets[1].split("/")[-1][2:])):
